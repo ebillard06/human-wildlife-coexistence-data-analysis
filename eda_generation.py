07713f5268a5d30682098a42829ed9d9aaf87f52
ebillard06 <ebillard06@gmail.com>
2023-02-02 11:47:45 -0500

    Create eda_generation.py

---
+++ eda_generation.py
@@ -0,0 +1,46 @@
+#I did not have success generating the profile report from 
+#within Google Colab or from directly in terminal, but I was 
+#able to generate and export to html from Jupyter Notebook.
+#Installed Anaconda package including Jupyter Notebook 
+#from https://www.anaconda.com/products/distribution
+
+#Ran in terminal before launching Jupyter Notebook
+#jupyter nbextension enable --py widgetsnbextension
+#pip install --upgrade --force-reinstall pandas
+#pip install ydata-profiling
+#pip install pandas-profiling
+
+
+import pandas as pd
+from pandas_profiling import ProfileReport 
+
+
+CompleteData = pd.read_csv("/Users/nerdbear/Documents/GitHub/human-wildlife-coexistence-data-analysis/CompleteData.csv", index_col=0, dtype=str)
+profile = ProfileReport(CompleteData, title="Human-Animal Coexistence Data - EDA Report")
+
+#To view report inside Jupyter Notebook:
+profile
+
+#To export report to html:
+profile.to_file("Human-Animal_Coexistence_Data_EDA_Report.html")
+
+#In Jupyter, go to "Files" and then go to your root folder
+#to locate the generated html document.
+
+### To Do:
+#1.  modify "CompleteData" so the rows that were not a part of "animals3" 
+#	 (there are only about 3) have the same "UniqueID" format as all other rows
+#2.  modify "CompleteData" to remove the columns that don't 
+#    need to be part of the final dataset (the ones i created for the merging)
+#3.  Modify how I read_csv into Jupyter for EDA generation. Right now I am 
+#	 putting all columns as "str" but at least column should actually be "int"
+#    Should I be setting any columns as "categorical" at this point in time or 
+# 	 is that just later? Some of the categorical ones will be messed up becuase
+# 	 I made them lists by merging rows from the other datasets.
+#4.  Generate new EDA report based on changes made to "CompleteData". 
+#5.  Save .html EDA report to Github.
+#4.  Update README file in GitHub to explain how datasets were treated for the 
+#	 joining (if too long for README, put in a separate document that is linked 
+# 	 in the README)
+#5.  Invite Professor Abdou to my Github. 
+#6.  Work on methodology
